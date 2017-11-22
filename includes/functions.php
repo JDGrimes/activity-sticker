416c9b39e5d80eb03f5e79fc972e956f8190d7da
JDGrimes <jdg@codesymphony.co>
2017-11-22 16:36:07 -0500

    Add fourth column on admin list table

--- includes/functions.php
+++ includes/functions.php
@@ -7,7 +7,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Restores a sticky_update to the good type
  *
- * @param int $id the activity id 
+ * @param int $id the activity id
  * @uses BP_Activity_Activity::save()
  */
 function activitysticker_restore_type( $id = 0 ) {
@@ -15,7 +15,7 @@ function activitysticker_restore_type( $id = 0 ) {
 		return;
 
 	$restored_activity = new BP_Activity_Activity( $id );
-				
+
 	if( !empty( $restored_activity ) ) {
 
 		switch( $restored_activity->component ) {
@@ -27,13 +27,13 @@ function activitysticker_restore_type( $id = 0 ) {
 			case 'bbpress' :
 				$restored_activity->type = 'bbp_topic_create';
 				break;
-					
+
 			case 'activity' :
 				$restored_activity->type = 'activity_update';
 				break;
-			
+
 		}
-				
+
 		$restored_activity->save();
 	}
 
@@ -50,18 +50,18 @@ function activitysticker_restore_type( $id = 0 ) {
 function activitysticker_is_stickable( $id = 0 ) {
 	if( empty( $id ) )
 		return false;
-	
+
 	$retval = true;
 	$maybe_sticky = new BP_Activity_Activity( $id );
-	
+
 	// first do we have the right component ?
 	if( !in_array( $maybe_sticky->component, array( 'activity', 'blogs', 'bbpress' ) ) )
 		$retval = false;
-		
+
 	//if so do we have the right type ?
 	if( !in_array( $maybe_sticky->type, array( 'sticky_update', 'activity_update', 'new_blog_post', 'bbp_topic_create' ) ) )
 		$retval = false;
-		
+
 	return $retval;
 }
 
@@ -69,9 +69,9 @@ function activitysticker_is_stickable( $id = 0 ) {
 /**
  * Gets the sticky_update from options table and returns it
  *
- * This is the tricky part! it builds a similar object to 
+ * This is the tricky part! it builds a similar object to
  * BP_Activity_Template class
- *  
+ *
  * @uses bp_get_option()
  * @uses bp_core_get_user_email()
  * @uses bp_core_get_username()
@@ -79,12 +79,12 @@ function activitysticker_is_stickable( $id = 0 ) {
  */
 function activitysticker_get_sticky_update() {
 	$sticky = bp_get_option( '_activitysticker_activity' );
-	
+
 	if( empty( $sticky ) || !is_array( $sticky ) )
 		return false;
-	
+
 	$sticky_activity = $sticky['activity_content'];
-	
+
 	if( empty( $sticky_activity ) || !is_object( $sticky_activity ) )
 		return false;
 
@@ -105,7 +105,7 @@ function activitysticker_get_sticky_update() {
 	$sticky_update->disable_blogforum_replies = 1;
 
 	$sticky_update->activity = $sticky_activity;
-	
+
 	return $sticky_update;
 }
 
@@ -114,7 +114,7 @@ function activitysticker_get_sticky_update() {
  * Sets a new sticky update
  *
  * @param string $content the activity content
- * @param string $redirect_error the url to redirect to in case of error 
+ * @param string $redirect_error the url to redirect to in case of error
  * @param string $redirect_success the url to redirect to in case of success
  * @uses buddypress()
  * @uses bp_loggedin_user_id()
@@ -130,15 +130,15 @@ function activitysticker_get_sticky_update() {
 function activitysticker_set_sticky_update( $content, $redirect_error = '', $redirect_success = '' ) {
 	if( empty( $redirect_error ) || empty( $redirect_success ) )
 		return false;
-	
+
 	$bp = buddypress();
-	
+
 	$user_id = bp_loggedin_user_id();
 	$from_user_link   = bp_core_get_userlink( $user_id );
 	$activity_action  = sprintf( __( '%s posted an update', 'activity-sticker' ), $from_user_link );
 	$activity_content = $content;
 	$primary_link     = bp_core_get_userlink( $user_id, false, true );
-	
+
 	$sticky_id = bp_activity_add( array(
 		'user_id'      => $user_id,
 		'action'       => apply_filters( 'bp_activity_new_update_action', $activity_action ),
@@ -147,37 +147,37 @@ function activitysticker_set_sticky_update( $content, $redirect_error = '', $red
 		'component'    => $bp->activity->id,
 		'type' => 'sticky_update'
 		) ) ;
-	
+
 	if( !empty( $sticky_id ) ) {
-		
+
 		$sticky = array( 'activity_id' => intval( $sticky_id ) );
 		$sticky_activity = new BP_Activity_Activity( $sticky_id );
-		
+
 		if( !empty( $sticky_activity ) ) {
 			$sticky['activity_content'] = $sticky_activity;
-			
+
 			// we need to check for a previous sticky and make it a regular activity update !
 			$previous_sticky = bp_get_option( '_activitysticker_activity' );
-			
+
 			if( !empty( $previous_sticky ) && is_array( $previous_sticky ) ) {
-				
+
 				activitysticker_restore_type( intval( $previous_sticky['activity_id'] ) );
-				
+
 			}
-			
+
 			bp_update_option( '_activitysticker_activity', $sticky );
-			
+
 			$redirect_success = add_query_arg( 'updated', $sticky_id, $redirect_success );
-			
+
 			wp_redirect( apply_filters( 'activitysticker_admin_success_redirect', $redirect_success ) );
 			exit;
-			
+
 		} else {
 			$redirect_error = add_query_arg( 'error', '2', $redirect_error );
 			wp_redirect( apply_filters( 'activitysticker_admin_error_redirect', $redirect_error ) );
 			exit;
 		}
-		
+
 	} else {
 		$redirect_error = add_query_arg( 'error', '3', $redirect_error );
 		wp_redirect( apply_filters( 'activitysticker_admin_error_redirect', $redirect_error ) );
@@ -238,7 +238,7 @@ function activitysticker_unset_sticky( $activity_id, $redirect_success = '' ) {
 function activitysticker_admin_output_sticky( $sticky_update = false ) {
 	if( empty( $sticky_update ) )
 		return false;
-		
+
 	$checkbox = sprintf( '<input type="checkbox" name="aid[]" value="%d" />', (int) $sticky_update->activity->id );
 
 	$authortd = '<strong>' . get_avatar( $sticky_update->activity->user_id, '32' ) . ' ' . bp_core_get_userlink( $sticky_update->activity->user_id ) . '</strong>';
@@ -294,6 +294,9 @@ function activitysticker_admin_output_sticky( $sticky_update = false ) {
 					<span class="unstick"><?php echo $actions['unstick'];?></span>
 				</div>
 			</td>
+			<td class="action column-action">
+				Sticky activity
+			</td>
 			<td class="response column-response">
 				<?php echo $viewtd;?>
 			</td>
@@ -315,10 +318,10 @@ function activitysticker_admin_output_sticky( $sticky_update = false ) {
  */
 function activitysticker_front_output_sticky( $sticky_update = false ) {
 	global $activities_template;
-	
+
 	if( empty( $sticky_update ) )
 		return false;
-		
+
 	$activities_template = $sticky_update;
 
 	remove_filter( 'bp_get_activity_content_body', 'bp_activity_truncate_entry', 5 );
@@ -371,22 +374,22 @@ function activity_sticker_parse_bp_cookie( $scope = 'mentions' ) {
  */
 function activitysticker_maybe_filter() {
 	$filter = true;
-	
+
 	if( bp_is_activity_component() && bp_is_current_action( 'p' ) )
 		$filter = false;
-		
+
 	if( bp_is_single_activity() )
 		$filter = false;
 
 	if( ( bp_is_user_activity() && bp_is_current_action( 'mentions' ) ) || ( activity_sticker_parse_bp_cookie() && bp_is_activity_component() && !bp_displayed_user_id() ) )
 		$filter = false;
-		
+
 	if( ( bp_is_user_activity() && bp_is_current_action( 'favorites' ) ) || ( activity_sticker_parse_bp_cookie( 'favorites' ) && bp_is_activity_component() && !bp_displayed_user_id() ) )
 		$filter = false;
-		
+
 	if( is_admin() && !empty( $_REQUEST['action'] ) && in_array( $_REQUEST['action'], array( 'edit', 'delete' ) ) )
 		$filter = false;
-	
+
 	return $filter;
 }
 
@@ -394,7 +397,7 @@ function activitysticker_maybe_filter() {
 /**
  * Updates the content of a sticky update or changes the type of an activity for a sticky one
  *
- * @param object $activity 
+ * @param object $activity
  * @uses bp_get_option()
  * @uses activitysticker_restore_type()
  * @uses bp_update_option()
@@ -422,30 +425,30 @@ function activitysticker_maybe_update_sticky( $activity = false ) {
 
 	} elseif( $activity->type != 'sticky_update' && $activity->id == $sticky_id ) {
 		//just delete the option
-		bp_delete_option( '_activitysticker_activity' );	
+		bp_delete_option( '_activitysticker_activity' );
 	}
-	
+
 }
 
 
 /**
  * Eventually deletes a sticky
  *
- * @param int $activity_id 
+ * @param int $activity_id
  * @uses bp_get_option()
  * @uses bp_delete_option()
  */
 function activitysticker_maybe_delete_sticky( $activity_id = 0 ) {
 	if( empty( $activity_id ) )
 		return;
-		
+
 	$previous_sticky = bp_get_option( '_activitysticker_activity' );
-	
+
 	if( !empty( $previous_sticky ) && is_array( $previous_sticky ) ) {
 		$sticky_id = intval( $previous_sticky['activity_id'] );
-		
+
 		if( $sticky_id == $activity_id )
 			bp_delete_option( '_activitysticker_activity' );
 	}
-	
+
 }
